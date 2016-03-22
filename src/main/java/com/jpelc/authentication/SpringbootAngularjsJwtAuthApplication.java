e94b2e1ef30eee223ffe3e81db707f37359af84f
Jakub Pelc <jakubspelc@gmail.com>
2016-03-22 18:34:49 +0100

    spring boot project structure initialized

---
+++ src/main/java/com/jpelc/authentication/SpringbootAngularjsJwtAuthApplication.java
@@ -0,0 +1,12 @@
+package com.jpelc.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularjsJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularjsJwtAuthApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jpelc/authentication/SpringbootAngularjsJwtAuthApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jpelc.authentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringbootAngularjsJwtAuthApplication.class)
+@WebAppConfiguration
+public class SpringbootAngularjsJwtAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
